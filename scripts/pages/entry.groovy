dd313b3b73a477f333fffa2fa3a6500df7442297
admin admin <evaladmin@example.com>
2019-10-16 01:45:19 -0400

    User admin wrote content /scripts/pages/entry.groovy

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -2,5 +2,6 @@ if(params.markdown) {
     response.setContentType("text/plain")
 }
 else { 
-    request.getRequestDispatcher("/static-assets/markup/index.html").forward(request, response);
+    //request.getRequestDispatcher("/static-assets/markup/index.html").forward(request, response);
+    response.redirect("/static-assets/markup/index.html");
 }
